3442068f198dadb6d7c909fbd142894d51a2ff8c
Tyler Coats <coatsctyler@gmail.com>
2023-05-06 20:30:45 -0400

    Coded home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,22 @@
+
+
+
 const Home = () => {
   return (
-    <div>
-        
+    <div className="homeCtn">
+        <div className="homeLeft">
+            <div className="leftHero">
+                <h1>Elevate Your Mind, Body, and Spirit with CRYSTAL*GEMS</h1>
+                <h2>Discover the Beauty and Power of Natural Gemstones</h2>
+            </div>
+            <div className="heroBtns">
+                <button>LEARN MORE</button>
+                <button>CATALOGUE</button>
+            </div>
+        </div>
+        <div className="homeRight">
+            <img src="https://bigquartz.com/media/wysiwyg/banner_crystal_spheres_points_1300_500.jpg" alt="" />
+        </div>
     </div>
   )
 }
